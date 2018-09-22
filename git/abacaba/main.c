8a3b714bad6f82a9ede620e0f46bd265a6c5cffb
Vlad <vlad.miryaha@gmail.com>
2018-09-22 21:52:05 +0300

    ok. read comments

--- git/abacaba/main.c
+++ git/abacaba/main.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+// generateString ? чтобы слова в названии как-то разделялись
 void generatestring(int n, char* mass)
 {
     mass[0] = 'a';
     char * temp = (char*)malloc(((1 << n) - 1) * sizeof(char));
     char * temp2[2];
+    // тут вы надеетесь, что temp2[1] == '\0' ... компилятор не обязательно сделает это за вас
     for (int i = 0; i < n; i++)
     {
         temp2[0] = 'a' + i + 1;
